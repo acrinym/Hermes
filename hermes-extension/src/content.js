7ca31ff641451f47b292b0d8d238d8468a537a77
acrinym <acrinym@gmail.com>
2025-06-10 13:54:55 -0400

    feat: auto generate site configs

--- hermes-extension/src/content.js
+++ hermes-extension/src/content.js
@@ -1,11 +1,13 @@
 import { initUI } from './ui.ts';
 import { initEffects } from './effects.ts';
 import { initMacros } from './macros.ts';
+import { ensureSiteConfig } from './domScanner.ts';
 
 export function init() {
   initUI();
   initEffects();
   initMacros();
+  ensureSiteConfig();
 }
 
 document.addEventListener('DOMContentLoaded', init);
