741bae95e589ee7eacd2c4e641d8a832ed9d3ee6
acrinym <acrinym@gmail.com>
2025-06-17 21:14:00 -0400

    Fix extension build

--- hermes-extension/src/ui.ts
+++ hermes-extension/src/ui.ts
@@ -258,6 +258,9 @@ function updateMacroSubmenuContents(menu: HTMLElement) {
   const exportBtn = createSubButton('Export All Macros...', () => exportMacros());
   menu.appendChild(exportBtn);
   }
+
+}
+
 }
 
 // === Macro Editor Panel ===
